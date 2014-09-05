4b6ad576177e4f4ce7cd1a8f7a80a1be0c786ee7
Dan Stevens <stevens@earthit.com>
2014-09-05 14:52:28 -0500

    Keep underscores in names when converting to postgresish.

--- lib/EarthIT/DBC/PostgresNamer.php
+++ lib/EarthIT/DBC/PostgresNamer.php
@@ -2,25 +2,25 @@
 
 /**
  * Names things in the typical Postgres fashion, which is
- * alllowercaseandsquishedtogether
+ * alllowercaseandsquishedtogetherwith_optional_underscores,
+ * (underscores are kept if they're part of the original name)
  */
 class EarthIT_DBC_PostgresNamer implements EarthIT_DBC_Namer
 {
-	protected function toLowerCase( $phrase ) {
-		// Ignoring non-US-English chars for now
-		return preg_replace('/[^a-z0-9]/', '', strtolower($phrase));
+	protected function squish( $phrase ) {
+		return preg_replace('/[^a-z0-9_]/', '', strtolower($phrase));
 	}
 	
 	public function getTableName( EarthIT_Schema_ResourceClass $c ) {
-		return $this->toLowerCase($c->getName());
+		return $this->squish($c->getName());
 	}	
 	public function getColumnName( EarthIT_Schema_ResourceClass $c, EarthIT_Schema_Field $f ) {
-		return $this->toLowerCase($f->getName());
+		return $this->squish($f->getName());
 	}
 	public function getIndexName( EarthIT_Schema_ResourceClass $c, EarthIT_Schema_Index $i ) {
-		return $this->toLowerCase($c->getName());
+		return $this->squish($c->getName());
 	}
 	public function getForeignKeyName( EarthIT_Schema_ResourceClass $c, EarthIT_Schema_Reference $r ) {
-		return $this->toLowerCase($c->getName()).$this->toLowerCase($r->getName());
+		return $this->squish($c->getName()).$this->squish($r->getName());
 	}
 }
