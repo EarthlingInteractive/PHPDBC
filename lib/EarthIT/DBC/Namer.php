bc8c3813272415cbb3ade029e246a52f37384e4a
Dan Stevens <stevens@earthit.com>
2015-03-06 13:06:54 -0600

    Fix parameter names of methods declared in EarthIT_DBC_Namer.

--- lib/EarthIT/DBC/Namer.php
+++ lib/EarthIT/DBC/Namer.php
@@ -2,8 +2,8 @@
 
 interface EarthIT_DBC_Namer
 {
-	public function getTableName( EarthIT_Schema_ResourceClass $c );
-	public function getColumnName( EarthIT_Schema_ResourceClass $c, EarthIT_Schema_Field $c );
-	public function getIndexName( EarthIT_Schema_ResourceClass $c, EarthIT_Schema_Index $c );
-	public function getForeignKeyName( EarthIT_Schema_ResourceClass $c, EarthIT_Schema_Reference $c );
+	public function getTableName( EarthIT_Schema_ResourceClass $rc );
+	public function getColumnName( EarthIT_Schema_ResourceClass $rc, EarthIT_Schema_Field $f );
+	public function getIndexName( EarthIT_Schema_ResourceClass $rc, EarthIT_Schema_Index $i );
+	public function getForeignKeyName( EarthIT_Schema_ResourceClass $rc, EarthIT_Schema_Reference $r );
 }
