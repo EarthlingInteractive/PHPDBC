a534d6b9bb05563492eb9b913dae213ea7b048f9
Jamie Role <role@earthlinginteractive.com>
2025-07-07 17:30:49 -0500

    Making PHP 8.4 compliant.

--- lib/EarthIT/DBC/SQLResult.php
+++ lib/EarthIT/DBC/SQLResult.php
@@ -11,19 +11,19 @@ interface EarthIT_DBC_SQLResult extends IteratorAggregate
 	 *   should return an empty array if the query returned no rows.
 	 */
 	public function getRows(); 
-	
+
 	/**
 	 * @return int for queries that modifiy the DB, the number of affected rows,
 	 *   0 for pure selects, and null if unknown.
 	 */
 	public function getAffectedRowCount();
-	
+
 	/**
 	 * If new records with auto-incremented IDs were added, this should return
 	 * the latest such ID.
 	 */
 	public function getLastInsertId();
-	
+
 	/**
 	 * @return Iterator that will iterate over the result rows.
 	 *   This can be expected to be called only once and not in combination
